b544eb89f3fb4105d09f9273ccdf207ae5b2380b
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-02-24 12:48:32 -0300

    Criando componente com a barra de experiencia e estilizando.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { Button } from './components/Button'
-
+import React from "react";
+import './styles/global.css';
+import { ExperienceBar } from "./components/experienceBar";
 
 function App() {
   return (
-    <>
-      <Button color="red"> Botão 1 </Button>
-      <Button color="blue"> Botão 2 </Button>
-      <Button color="white"> Botão 3 </Button>
-    </>
+    <div className="container">
+     <ExperienceBar/>
+    </div>
   );
 }
 
