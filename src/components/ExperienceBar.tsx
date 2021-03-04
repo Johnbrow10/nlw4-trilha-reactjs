4a871f1f62a32fddf6c4a87b6776bb640c986f7d
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-04 16:39:44 -0300

    Criando calculo para estimular quanta experiencia falta  a subur de niver e assim demostrar na barra que estar no front-end

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,15 +1,24 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
+
+    const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
+    // calculo para saber a porcentagem para adquirir experiencia
+    const percentNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
     return (
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{ width: '50%' }} />
+                <div style={{ width: `${percentNextLevel}%` }} />
 
-                <span className={styles.currentExperience} style={{ left: '50%'}}> 300xp</span>
+                <span className={styles.currentExperience} style={{ left: `${percentNextLevel}%` }}>
+                    {currentExperience} xp
+                </span>
             </div>
-            <span>600 xp</span>
+            <span>{experienceToNextLevel} xp</span>
         </header>
     )
 }
