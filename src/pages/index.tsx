81af55aaaa1cbe31b1e0e3e49bcd9d7bbf0cc707
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-08 16:31:37 -0300

    Criação da interface HomeProps para tipars as variaveis das props, colocando em volta do corpo principal o ChallengesProvider, carregando as variaveis do cookies na index.ts agora e tipando novamente em Number as variaveis do props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,63 @@
-import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import Head from "next/head";
+import { GetServerSideProps } from "next";
 import { ChallengeBox } from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import { CountdownProvider } from '../contexts/CountdownContext';
-import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from "../contexts/CountdownContext";
+import styles from "../styles/pages/Home.module.css";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
 
-
-
-export default function Home(props) {
-  console.log(props)
+export default function Home(props: HomeProps) {
+  console.log(props);
 
   return (
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Inicio | move.it</title>
+        </Head>
 
-    <div className={styles.container}>
-      <Head>
-        <title>Inicio | move.it</title>
-      </Head>
+        <ExperienceBar />
 
-      <ExperienceBar />
-
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
-
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   );
 }
 
 // Controlador de dados, ele tem a função de escolher quais os dados que serao
 //  passado da camada de back para o front e vice versa
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-
   const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
-
 
   return {
     props: {
-      level,
-      currentExperience,
-      challengesCompleted
-    }
-  }
-}
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  };
+};
